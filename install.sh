06c54d16749b68ee1c1b624afb86abab6c717a44
Lion <11315889@qq.com>
2012-09-14 16:26:00 +0800

    1

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Running Openstack Dashboard hacker..."
 #copy files
 cp -Rf ./horizon /usr/share/pyshared/
 
@@ -7,13 +8,15 @@ sed -i 's/for pool in api.floating_ip_pools_list(self.request)]/for pool in api.
 
 #Create pythton package
 mkdir -p /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd
-ln -s /usr/share/pyshared/horizon/dashboards/settings/passwd/* /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd/
+ln -sf /usr/share/pyshared/horizon/dashboards/settings/passwd/* /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd/
 mkdir -p /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/templates/settings/passwd
-ln -s /usr/share/pyshared/horizon/dashboards/settings/templates/settings/passwd/* /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/templates/settings/passwd/
+ln -sf /usr/share/pyshared/horizon/dashboards/settings/templates/settings/passwd/* /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/templates/settings/passwd/
 
 #Create 
 #Change for passwd  /usr/share/pyshared/horizon/dashboards/settings/dashboard.py
 sed -i "s/panels = ('user', 'project'/panels = ('user', 'passwd', 'project'/g" /usr/share/pyshared/horizon/dashboards/settings/dashboard.py
 
 #restart apache
-service apach2 restart
+service apache2 restart
+
+echo "All processes has  finished. "
