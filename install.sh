7997bbc897f4f583a3e837ea8b8613a341f79368
Lion <11315889@qq.com>
2012-09-14 15:36:35 +0800

    Unix 换行符

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
+#!/bin/bass
 cp -Rf ./horizon /usr/share/pyshared/
 
 #floating ips view.py
 sed -i 's/for pool in api.floating_ip_pools_list(self.request)]/for pool in api.keystone.tenant_list(self.request)]/g' /usr/share/pyshared/horizon/dashboards/nova/access_and_security/floating_ips/views.py
-
 
 mkdir -p /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd
 ln -s /usr/share/pyshared/horizon/dashboards/settings/passwd/* /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd/
