ae6f920b441c4aa15b434b4c95b28348b4b09bf7
Lion <11315889@qq.com>
2012-09-14 15:20:14 +0800

    1

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 cp -Rf ./horizon /usr/share/pyshared/
 
+#floating ips view.py
 sed -i 's/for pool in api.floating_ip_pools_list(self.request)]/for pool in api.keystone.tenant_list(self.request)]/g' /usr/share/pyshared/horizon/dashboards/nova/access_and_security/floating_ips/views.py
 
 
-mkdir /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd
+mkdir -p /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd
 ln -s /usr/share/pyshared/horizon/dashboards/settings/passwd/* /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/passwd/
-mkdir /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/templates/settings/passwd
+mkdir -p /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/templates/settings/passwd
 ln -s /usr/share/pyshared/horizon/dashboards/settings/templates/settings/passwd/*.html /usr/lib/python2.7/dist-packages/horizon/dashboards/settings/templates/settings/passwd/
 
-#修改 /usr/share/pyshared/horizon/dashboards/settings/dashboard.py
+#Change for passwd  /usr/share/pyshared/horizon/dashboards/settings/dashboard.py
 sed -i "s/panels = ('user', 'project'/panels = ('user', 'passwd', 'project'/g" /usr/share/pyshared/horizon/dashboards/settings/dashboard.py
